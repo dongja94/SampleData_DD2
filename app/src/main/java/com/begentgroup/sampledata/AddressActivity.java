bbfe5d70c591a72a6093bd13d7d3cf853887dbdb
Yon Seungik <dongja94@gmail.com>
2016-08-11 11:34:42 +0900

    simple cursor adapter

--- app/src/main/java/com/begentgroup/sampledata/AddressActivity.java
+++ app/src/main/java/com/begentgroup/sampledata/AddressActivity.java
@@ -1,35 +1,76 @@
 package com.begentgroup.sampledata;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.ListView;
-
-import java.util.List;
+import android.widget.TextView;
 
 public class AddressActivity extends AppCompatActivity {
 
     ListView listView;
-    ArrayAdapter<Person> mAdapter;
+    //    ArrayAdapter<Person> mAdapter;
+    SimpleCursorAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address);
-        listView = (ListView)findViewById(R.id.listView);
-        mAdapter = new ArrayAdapter<Person>(this, android.R.layout.simple_list_item_1);
+        listView = (ListView) findViewById(R.id.listView);
+//        mAdapter = new ArrayAdapter<Person>(this, android.R.layout.simple_list_item_1);
+        String[] from = {AddressContract.Address.COLUMN_NAME,
+                AddressContract.Address.COLUMN_AGE,
+                AddressContract.Address.COLUMN_PHONE,
+                AddressContract.Address.COLUMN_ADDRESS};
+        int[] to = {R.id.text_name,
+                R.id.text_age,
+                R.id.text_phone,
+                R.id.text_address};
+        mAdapter = new SimpleCursorAdapter(this, R.layout.view_item, null, from, to, 0);
+        mAdapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
+            @Override
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                if (columnIndex == phoneIndex) {
+                    String phone = cursor.getString(columnIndex);
+                    TextView tv = (TextView)view;
+                    StringBuilder sb = new StringBuilder();
+                    if (phone.length() > 0) {
+                        sb.append(phone.charAt(0));
+                    }
+                    for (int i = 1; i < phone.length(); i++) {
+                        sb.append("*");
+                    }
+                    tv.setText(sb.toString());
+                    return true;
+                }
+                return false;
+            }
+        });
         listView.setAdapter(mAdapter);
     }
+
+    int phoneIndex = -1;
 
     @Override
     protected void onStart() {
         super.onStart();
-        mAdapter.clear();
-        List<Person> list = DBManager.getInstance().getPersonList(null);
-        mAdapter.addAll(list);
+//        mAdapter.clear();
+//        List<Person> list = DBManager.getInstance().getPersonList(null);
+//        mAdapter.addAll(list);
+        Cursor c = DBManager.getInstance().getPersonCursor(null);
+        phoneIndex = c.getColumnIndex(AddressContract.Address.COLUMN_PHONE);
+        mAdapter.changeCursor(c);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAdapter.changeCursor(null);
     }
 
     @Override
